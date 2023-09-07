a7065cb341e506335ecbd99032ea7c1a9d62428c
jasony000 <jasony1004@gmail.com>
2023-09-06 21:26:30 -0400

    forgot to save server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   // statically serve everything in the build folder on the route '/build'
-  app.use('/build', express.static(path.join(__dirname, '../build/')));
+  app.use('/', express.static(path.join(__dirname, '../build/')));
   // serve index.html on the route '/'
   // app.get('/', (req, res) => {
   //   return res.status(200).sendFile(path.join(__dirname, 'index.html'));
