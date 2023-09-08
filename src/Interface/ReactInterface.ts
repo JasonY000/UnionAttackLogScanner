a741c8cfed552e73b3192a1b21bacf12c26f7aec
jasony000 <jasony1004@gmail.com>
2023-09-08 17:20:29 -0400

    started the creation on config, allowing user to add member on the app.

---
+++ src/Interface/ReactInterface.ts
@@ -0,0 +1,23 @@
+//App.tsx
+export interface chartProp {
+  [key: string]: MemberPerformance;
+}
+export interface MemberPerformance {
+  attack: number;
+  damage: number;
+}
+//AttackRow.tsx
+export interface RowProp {
+  num: number;
+  members: string[];
+}
+//Charts.tsx
+export interface DataForChart {
+  name: string;
+  damage: number;
+}
+
+export interface AddMemberProp {
+  members: Set<string>;
+  setMembers: (newSet: Set<string>) => void;
+}
---
+++ src/components/Settings.tsx
@@ -0,0 +1,33 @@
+import '../scss/App.scss';
+import React, { useState, useEffect, useRef } from 'react';
+import AddMemeberComp from './ConfigAddmember';
+interface settingsProp {
+  Close: () => void;
+}
+const Settings: React.FC<settingsProp> = ({ Close }) => {
+  const [members, setMembers] = useState<Set<string>>(new Set<string>());
+  function setMembersFunc(set: Set<string>) {
+    setMembers(set);
+  }
+
+  return (
+    <div>
+      <div className='pop-up popupbackground'></div>
+      <div className='pop-up note'>
+        <div className='popUpTop'>
+          <h1>Config</h1>
+          <button onClick={Close}>X</button>
+        </div>
+        <div className='mainDiv'>
+          <AddMemeberComp members={members} setMembers={setMembersFunc} />
+          <div>
+            <input placeholder='name'></input>
+            <button>add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
